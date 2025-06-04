f28ab69262dff65b3b2945b4fcd8cd9b427c5615
Jaime <jai.hernandeza@duocuc.cl>
2025-06-04 14:10:02 -0400

    Eliminé el Bean

--- src/main/java/com/servicio/usuario/UsuarioApplication.java
+++ src/main/java/com/servicio/usuario/UsuarioApplication.java
@@ -2,8 +2,6 @@ package com.servicio.usuario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class UsuarioApplication {
@@ -11,10 +9,5 @@ public class UsuarioApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UsuarioApplication.class, args);
 	}
-
-	@Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
 }
 
